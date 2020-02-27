90d82b68bf1161a3ae4bd11cf61116b1d2c1efcd
joao <joao.paulo@g44.com.br>
2020-02-27 08:45:29 -0300

    Third lesson

--- contracts/Emeraldarium.sol
+++ contracts/Emeraldarium.sol
@@ -4,13 +4,26 @@ contract Emeraldarium {
   string public name = "Emeraldarium";
   string public standard = "Emeraldarium v1.0";
   string public symbol = "EMD";
-
   uint256 public totalSupply;
+
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
 
   mapping(address => uint256) public balanceOf;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success){
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
