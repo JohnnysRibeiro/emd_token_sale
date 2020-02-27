78e4725847440ecf4162bcf528a29407dafb1283
joao <joao.paulo@g44.com.br>
2020-02-27 10:59:23 -0300

    Fourth lesson

--- contracts/Emeraldarium.sol
+++ contracts/Emeraldarium.sol
@@ -12,7 +12,14 @@ contract Emeraldarium {
     uint256 _value
   );
 
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -20,10 +27,19 @@ contract Emeraldarium {
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success){
-    require(balanceOf[msg.sender] >= _value);
+    require(balanceOf[msg.sender] >= _value, "Not enough funds");
+
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
-    Transfer(msg.sender, _to, _value);
+
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 }
